be3ea807d67940cf8bde8c8c2c8113b5e2b1504b
Matthew Zimmer <M16r01@gmail.com>
2017-06-06 00:24:20 -0400

    Implemented bare bones PID controller. Car steers for a few seconds and veers off into the ditch. WIP.

--- src/PID.cpp
+++ src/PID.cpp
@@ -11,11 +11,18 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+    this->Kp = Kp;
+    this->Ki = Ki;
+    this->Kd = Kd;
 }
 
 void PID::UpdateError(double cte) {
+    p_error = cte;
+    i_error += cte;
+    d_error = cte-d_error;
 }
 
 double PID::TotalError() {
+    return -Kp*p_error - Ki*i_error - Kd*d_error;
 }
 
