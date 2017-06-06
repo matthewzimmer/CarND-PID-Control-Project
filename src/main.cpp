10085457072de4858df5bddc8c51b9f27f75efe8
Matthew Zimmer <M16r01@gmail.com>
2017-06-06 01:18:19 -0400

    Even better Kp, Ki, and Kd hyperparameters so car can drive at 50 MPH around track. This project is very addicting!

--- src/main.cpp
+++ src/main.cpp
@@ -34,7 +34,7 @@ int main() {
 
     PID pid;
     // TODO: Initialize the pid variable.
-    pid.Init(0.05, 0.004, 50.0);
+    pid.Init(0.15, 0.0009, 4.0);
 
     h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
         // "42" at the start of the message means there's a websocket message event.
@@ -65,7 +65,7 @@ int main() {
 
                     json msgJson;
                     msgJson["steering_angle"] = steer_value;
-                    msgJson["throttle"] = 0.3;
+                    msgJson["throttle"] = 0.5;
                     auto msg = "42[\"steer\"," + msgJson.dump() + "]";
                     std::cout << msg << std::endl;
                     ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
